b054016d1f54fa242ba1997ea0e9bbfce3973e74
ahmedtarekwork <ahmedtareka777@gmail.com>
2025-03-27 17:20:50 +0200

    Solve AuthBtns.tsx responsive issue

--- frontend/src/components/layout/Header/AuthBtns.tsx
+++ frontend/src/components/layout/Header/AuthBtns.tsx
@@ -21,13 +21,17 @@ const AuthBtns = ({ ItemHolder, ...props }: Props) => {
       className={cn("flex max-sm:flex-col", props.className || "")}
     >
       <BtnItemHolder>
-        <Button variant="link" asChild className="btn-with-outer-border w-full">
+        <Button
+          variant="link"
+          asChild
+          className="btn-with-outer-border max-sm:w-full"
+        >
           <Link href="/login">Login</Link>
         </Button>
       </BtnItemHolder>
 
       <BtnItemHolder>
-        <Button asChild className="w-full">
+        <Button asChild className="max-sm:w-full">
           <Link href="/sign-up">Sign Up</Link>
         </Button>
       </BtnItemHolder>
